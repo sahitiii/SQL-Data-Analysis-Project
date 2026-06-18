73618f181828177a681e88b744a37c534ece8818
Sahiti Puppala <77136852+sahitiii@users.noreply.github.com>
2026-06-18 18:49:06 +0100

    customer report  for key customer metrics
    This SQL file generates a customer report that consolidates key metrics and segments customers into categories.

---
+++ Reports/customer_report.sql
@@ -0,0 +1,23 @@
+/*
+========================
+Customer Report
+=======================
+Purpose:
+This report consolidates key customer metrics and behaviours
+
+Highlights:
+
+1) Gathers essential fields such as names, ages and transaction details.
+2) Segments customers into categories(VIP, regular, New) and age groups.
+3) Aggregates customer level metrics:
+-Total orders
+-Total Sales
+-Total quantity purchased
+-Lifespan(in months)
+4) Calculates valueable KPIs:
+-recency(months since last order)
+-average order value
+-average monthly spend
+*/
+
+--STEP 1: Retriving core columns from tables
